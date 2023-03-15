ce7974daae94a403338a78689eb7f7d1de66cdc2
unknown <Lintheshwar>
2023-03-15 21:11:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter,Route } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Coinpage from './Pages/Coinpage';
+import Homepage from './Pages/Homepage';
+
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header/>
+          <Route path="/" component={Homepage} exact/>
+          <Route path="/coins/:id" component={Coinpage}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
